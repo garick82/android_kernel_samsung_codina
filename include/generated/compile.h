eb233b009e016ed7e26cf2d474a990a980104c8e
bagnz0r <bagnohax@gmail.com>
2013-05-28 00:08:05 +0200

    Fixed changeset

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Fri May 24 16:20:37 CEST 2013"
-#define LINUX_COMPILE_BY "michal"
-#define LINUX_COMPILE_HOST "archhost"
-#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
